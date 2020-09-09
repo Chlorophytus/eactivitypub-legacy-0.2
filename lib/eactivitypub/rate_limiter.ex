594e19f84c51ef576ed30efe032ef7a9fbf5531d
Roland Metivier <metivier.roland@chlorophyt.us>
2020-09-09 09:18:02 -0500

    add this, broke the workspace

--- lib/eactivitypub/rate_limiter.ex
+++ lib/eactivitypub/rate_limiter.ex
@@ -13,19 +13,21 @@ defmodule Eactivitypub.RateLimiter do
     @constants %{initial_left: 2, grace_seconds: 60}
     @enforce_keys [:ref]
     defstruct ref: nil, hits: @constants[:initial_left]
+
     @type t :: %__MODULE__{ref: String.t(), hits: non_neg_integer}
   end
 
+  @impl true
   @spec init(any) :: {:ok, State.t()}
   def init(_) do
     {:ok, %State{ref: reference64()}}
   end
 
+  @spec reference64() :: binary
   @doc """
   Generates a Base64 variant of a SHA3-224 hashed Erlang unique integer.
   This is used for identifying rate limiters.
   """
-  @spec reference64() :: binary
   def reference64() do
     ref_integer = :erlang.unique_integer()
     Base.encode64(:crypto.hash(:sha3_224, to_charlist(ref_integer)))
